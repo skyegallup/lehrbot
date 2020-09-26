377c020dd30e1b350ec2dc272bb258aaa2b1a116
Colby Gallup <colbygallup@gmail.com>
2020-09-26 13:44:20 -0400

    Added framework stuff

--- lehrbot.py
+++ lehrbot.py
@@ -1,1 +1,32 @@
+from typing import List
+
 import discord
+from discord.user import User
+from discord.message import Message
+
+
+client = discord.Client()
+
+
+async def joinqueue(user: User) -> None:
+    print(user.name)
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message: Message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$'):
+        tokens: List[str] = message.content[1:].split(' ')
+        if tokens[0] == 'joinqueue':
+            await joinqueue(message.author)
+        await message.channel.send('Hello!')
+
+
+client.run('NzU5NDU0ODE0NTUxMTQ2NTA2.X29vaQ.MuNF7XmF8mefy-_WzxB4vUYbAUM')
