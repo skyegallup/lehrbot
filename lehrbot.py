26e6996eae0a1b6cb16bee197b6c0ff46b36dd5d
dwm9636 <dwm9636@g.rit.edu>
2020-09-26 16:04:11 -0400

    help command

--- lehrbot.py
+++ lehrbot.py
@@ -3,6 +3,7 @@ from typing import List
 import discord
 from discord.user import User
 from discord.message import Message
+from discord.embeds import Embed
 
 
 client = discord.Client()
@@ -22,6 +23,15 @@ async def ready(mentor: User, channel) -> None:
     student: User = queue.pop(0)
     await channel.send(str(queue))
 
+async def help(channel):
+    embedVar = Embed(title = "Help!", description = "I need somebody to tell me valid commands", color = 0xf76902)
+    embedVar.add_field(name = "$joinqueue", value = "Add yourself to an existing queue", inline = False)
+    embedVar.add_field(name = "$showqueue", value = "Show the people currently in queue", inline = False)
+    embedVar.add_field(name = "$joinclass", value = "Add yourself to a class", inline = False)
+    embedVar.add_field(name = "$ready (admin only)", value = "Move to the next student in the queue", inline = False)
+    embedVar.add_field(name = "$makeclass (admin only)", value = "Create a class", inline = False)
+    embedVar.add_field(name = "$deleteclass (admin only)", value = "Deletes a class", inline = False)
+    await channel.send(embed = embedVar)
 
 @client.event
 async def on_ready():
@@ -39,7 +49,10 @@ async def on_message(message: Message):
             await joinqueue(message.author, message.channel)
         elif tokens[0] == 'ready':
             await ready(message.author, message.channel)
-        await message.channel.send('Hello!')
+        elif tokens[0] == 'help':
+            await help(message.channel)
+        else:
+            await message.channel.send("Invalid command. Type $help for more options")
 
 
 client.run('NzU5NDU0ODE0NTUxMTQ2NTA2.X29vaQ.MuNF7XmF8mefy-_WzxB4vUYbAUM')
